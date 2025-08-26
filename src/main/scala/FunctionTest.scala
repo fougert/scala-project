68cf5fc07aa09278020aa11c0f40ac6353a8dd18
luhuan <fougert@163.com>
2025-08-26 16:00:33 +0800

    函数测试

---
+++ src/main/scala/FunctionTest.scala
@@ -0,0 +1,31 @@
+/**
+ * @Author luhuan
+ * @create 2025/8/26 
+ * @description  函数测试
+ */
+object FunctionTest {
+
+  def main(args: Array[String]): Unit = {
+
+   /* def f(s: String): String = {
+      return s+ " test";
+    }
+
+    def f2(s: String): String =  s + " test";
+
+    println(f("hello"))
+    println(f2("hello"))*/
+
+
+
+
+  }
+
+
+  def operation(arr: Array[Int], op: Int => Int) = {
+    for (eml <- arr)
+  }
+
+
+
+}
--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,4 +1,4 @@
-
+import scala.util.control.Breaks
 
 object HelloWord {
 
@@ -6,8 +6,35 @@ object HelloWord {
    * scala 注释
    * @param args
    */
-  def main(args: Array[String]) = {
+/*  def main(args: Array[String]) = {
     println("Hello Word")
+  }*/
+
+  def main(args: Array[String]) = {
+    /*for (i <-1 to 3){
+      //1 到 3 循环 前后闭合
+      print(i + " ")
+    }
+
+    for ( i <- 1 until 3){
+      // 1 2 前闭后开
+      print(i + " ")
+    }*/
+
+    //循环步长
+    for (i <- 1 to 10 by 2) {
+      //1、3、5、7、9
+      print(i + " ")
+    }
+
+    Breaks().breakable(
+      for (i <- 1 to 10){
+        println(i + " ")
+        //结束循环
+        if (i == 5) Breaks.break()
+      }
+    )
+
   }
 
 }
